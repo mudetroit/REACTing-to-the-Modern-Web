69ec6ee07c6d42863cc8f629e676c8ff64a8425d
Matt LaForest <mudetroit@gmail.com>
2014-07-14 16:21:11 -0400

    Adding live updating elements to demonstrate data flow.

--- src/js/filterableProductTable.jsx
+++ src/js/filterableProductTable.jsx
@@ -1,13 +1,40 @@
+var _ = require('lodash');
 var React = require('react');
 var SearchBar = require('./searchBar');
 var ProductTable = require('./productTable');
 
-module.exports = React.createClass({
+module.exports  = React.createClass({
+    getInitialState: function() {
+        return {
+            filterText: '',
+            inStockOnly: false
+        };
+    },
+
+    handleUserInput: function(filterText, inStockOnly) {
+        this.setState({
+            filterText: filterText,
+            inStockOnly: inStockOnly
+        });
+    },
+
     render: function() {
+      var self = this;
+      var filteredProducts = _.where(this.props.products, function(product){
+        return (!self.state.inStockOnly || product.stocked)
+          && (!self.state.filterText || product.name.indexOf(self.state.filterText) > -1)
+      });
+
         return (
             <div>
-                <SearchBar />
-                <ProductTable products={this.props.products} />
+                <SearchBar
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                    onUserInput={this.handleUserInput}
+                />
+                <ProductTable
+                    products={filteredProducts}
+                />
             </div>
         );
     }
--- src/js/searchBar.jsx
+++ src/js/searchBar.jsx
@@ -1,15 +1,33 @@
 var React = require('react');
 
-module.exports = React.createClass({
-  render: function(){
-    return (
-      <form>
-          <input type="text" placeholder="Search..." />
-          <p>
-              <input id="inStock" type="checkbox" />
-              <label htmlFor="inStock">Only show products in stock</label>
-          </p>
-      </form>
-    );
-  }
-})
+module.exports =  React.createClass({
+    handleChange: function() {
+        this.props.onUserInput(
+            this.refs.filterTextInput.getDOMNode().value,
+            this.refs.inStockOnlyInput.getDOMNode().checked
+        );
+    },
+    render: function() {
+        return (
+            <form>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={this.props.filterText}
+                    ref="filterTextInput"
+                    onChange={this.handleChange}
+                />
+                <p>
+                    <input
+                        type="checkbox"
+                        value={this.props.inStockOnly}
+                        ref="inStockOnlyInput"
+                        onChange={this.handleChange}
+                    />
+                    Only show products in stock
+                </p>
+            </form>
+        );
+    }
+});
+
